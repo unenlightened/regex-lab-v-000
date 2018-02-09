d206bd1b54a0be6d43169f8faeeb14815760115a
Chely Ho <adept_inept@yahoo.com>
2018-02-09 05:06:08 +0000

    automatically backed up by learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -3,11 +3,11 @@ def starts_with_a_vowel?(word)
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-  text.match(/un+\w+ing/^\w{5}\s/)
+  text.match(/un+\w+ing//)
 end
 
 def words_five_letters_long(text)
-  text.scan(//)
+  text.scan(/^\w{5}\s/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
